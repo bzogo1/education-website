a73d9e8042dbc0bc65092694c59ab1b458ad3b34
Bogdan Zogovic <bzogovic7@gmail.com>
2026-09-09 11:08:56 +0000

    Revert "add new elements on home page"
    This reverts commit 17135789dcbed8db95238f549a11c9a4aa388ffe.

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -120,15 +120,7 @@ declare module 'astro:content' {
 		: any;
 
 	type DataEntryMap = {
-		"blog": Record<string, {
-  id: string;
-  body?: string;
-  collection: "blog";
-  data: InferEntrySchema<"blog">;
-  rendered?: RenderedContent;
-  filePath?: string;
-}>;
-
+		
 	};
 
 	type ExtractLoaderTypes<T> = T extends import('astro/loaders').LiveLoader<
@@ -157,6 +149,6 @@ declare module 'astro:content' {
 		LiveContentConfig['collections'][C]['loader']
 	>;
 
-	export type ContentConfig = typeof import("../src/content.config.js");
+	export type ContentConfig = never;
 	export type LiveContentConfig = never;
 }
