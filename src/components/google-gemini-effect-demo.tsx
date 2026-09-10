639bbb0bab47be23a4ad81b69517ab65dce8b25f
Bogdan Zogovic <bzogovic7@gmail.com>
2026-09-10 16:20:48 +0000

    repositioning icons element

--- src/components/google-gemini-effect-demo.tsx
+++ src/components/google-gemini-effect-demo.tsx
@@ -18,7 +18,7 @@ export default function GoogleGeminiEffectDemo() {
 
   return (
     <div
-      className="h-screen bg-black w-full dark:border dark:border-white/[0.1] rounded-md relative pt-40 overflow-clip"
+      className="h-[115vh] bg-black w-full dark:border dark:border-white/[0.1] rounded-md relative pt-40 overflow-clip"
       ref={ref}
     >
       <GoogleGeminiEffect
